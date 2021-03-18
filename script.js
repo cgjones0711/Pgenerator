af813eb17cffc3016a69be70566687ded25d1cbd
Clay Jones <cgjones0711@gmail.com>
2021-03-17 21:46:52 -0500

    added num / let variable

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwrdLength = 8;
+const upperCase="A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+var lowerCase="a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
+var symbols="!","@","#","$","%","&","?"
+var numbers="1","2","3","4","5","6","7","8","9",
+
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
+  
+
 
   passwordText.value = password;
 
@@ -14,4 +24,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-function arrayFromLowToHigh(low,high)
+
+
+
+
