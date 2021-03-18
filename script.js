fb6e0d4ddc977dac30fc55aec615bf0e614bbf31
Clay Jones <cgjones0711@gmail.com>
2021-03-17 23:06:05 -0500

    numberLength prompt

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwrdLength = 8;
-const upperCase="A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
-var lowerCase="a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
-var symbols="!","@","#","$","%","&","?"
-var numbers="1","2","3","4","5","6","7","8","9",
 
 
+function generatePassword(){
+
+  let numberLength = 0;
+  let upperCase = true;
+  let lowerCase = true;
+  let specialchar = true;
+  let number = true;
+  let alphaLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let alphaUpper =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  let special = ["!","@","#","$","%","&","?"];
+  let numero = ["1","2","3","4","5","6","7","8","9"];
+  
+  
+  
+  numberLength = prompt("How many characters do you want?");
+  if (isNaN(numberLength) || (8>numberLength) || (numberLength>128)) {
+    alert("Must be between a number of 8 and 128");
+    return;
+
+  }
+  else{
+    alert("You selected " + numberLength + ".");
+    
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
-  
-
-
   passwordText.value = password;
 
 }
